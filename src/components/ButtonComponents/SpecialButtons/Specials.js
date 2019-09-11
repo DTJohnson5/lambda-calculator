ab7bb21cc5ecba56283ecdfb7e26bd8e8a39e7cd
David.Johnson <davidt.johnson75@yahoo.com>
2019-09-10 19:30:34 -0500

    All MVP requirements met

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
+import SpecialButton from './SpecialButton'
+import {specials} from '../../../data'
+
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+const [spcValue] = useState(specials)
   return (
-    <div>
+    <div className='spcContainer'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {spcValue.map((special, i) => {
+         return <SpecialButton key={i} specials={special}/>
+       })}
     </div>
   );
 };
+
+export default Specials;
